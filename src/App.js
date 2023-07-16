610829fe8bd32b45a8e3dc17da5d33ea4e5c469b
gyueunnim <rnrmfrbdms@gmail.com>
2023-07-16 14:11:36 +0900

    firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/messaging';
 
 function App() {
+  function requestPermission() {
+    console.log("권한 요청 중...");
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("알림 권한이 허용됨");
+  
+        // FCM 메세지 처리
+      } else {
+        console.log("알림 권한 허용 안됨");
+      }
+    });
+  }
+
+  useEffect(() => {
+    requestPermission()
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>푸시 알림 테스트</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from 'firebase/app'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
+
+// Firebase 구성 정보 가져오기
+const firebaseConfig = {
+  apiKey: "AIzaSyAsgj_iDkXgoipFY9AUbuYNKQ1O7BiLcho",
+  authDomain: "react-push-test-58035.firebaseapp.com",
+  projectId: "react-push-test-58035",
+  storageBucket: "react-push-test-58035.appspot.com",
+  messagingSenderId: "644484203614",
+  appId: "1:644484203614:web:04b2927d12fdf1e592a0cf",
+  measurementId: "G-KLHNKZPZ4K",
+};
+
+// Firebase 앱 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firebase Messaging 초기화
+const messaging = getMessaging(app);
+
+getToken(messaging, { vapidKey: 'BI-_sWLOnaahPCqAvw8zFydQz1yR24QkOp817njCh_VWqLCGjdSbr3MtvjvHqbrz8D1cBXKdB8rab5O3hDoTTmk' }).then((currentToken) => {
+  if(currentToken) {
+    console.log('FCM 토큰: ', currentToken);
+  } else {
+    console.log('FCM 토큰이 없습니다.');
+  }
+}).catch((error) => {
+  console.log('FCM 토큰 가져오기 오류 : ', error);
+})
+
+onMessage(messaging, (payload) => {
+  console.log('Received foreground Message : ', payload.notification)
+  const { title, body } = payload.notification;
+  new Notification(title, { body })
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
